70a4904e8e6d63cbc513a7a2a544274a1b1622ee
midbel <brun.nicolas21@gmail.com>
2024-12-02 19:28:30 +0100

    script to help by scanning xml document

---
+++ scripts/scanxml.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"os"
+
+	"github.com/midbel/codecs/xml"
+)
+
+func main() {
+	flag.Parse()
+
+	r, err := os.Open(flag.Arg(0))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
+	defer r.Close()
+
+	scan := xml.Scan(r)
+	for {
+		tok := scan.Scan()
+		fmt.Println(tok)
+		if tok.Type == xml.EOF || tok.Type == xml.Invalid {
+			break
+		}
+	}
+}
