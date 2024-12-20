662ea68edef952866b1bbc47ca4635dbc139533c
midbel <brun.nicolas21@gmail.com>
2024-12-20 17:01:19 +0100

    result item

--- xml/list.go
+++ xml/list.go
@@ -5,6 +5,72 @@ import (
 	"slices"
 )
 
+type ResultItem interface {
+	Node() Node
+	Value() any
+}
+
+type literalItem struct {
+	value any
+}
+
+func createLiteral(value any) ResultItem {
+	return literalItem{
+		value: value,
+	}
+}
+
+func (i literalItem) Node() Node {
+	var (
+		qn = QName{
+			Name: "literal",
+		}
+		res = NewElement(qn)
+	)
+	str, _ := toString(i.value)
+	res.Append(NewText(str))
+	return res
+
+}
+
+func (i literalItem) Value() any {
+	return i.value
+}
+
+type nodeItem struct {
+	node Node
+}
+
+func createNode(node Node) ResultItem {
+	return nodeItem{
+		node: node,
+	}
+}
+
+func (i nodeItem) Node() Node {
+	return i.node
+}
+
+func (i nodeItem) Value() any {
+	var traverse func(Node) any
+	traverse = func(n Node) any {
+		el, ok := n.(*Element)
+		if !ok {
+			return n.Value()
+		}
+		var arr []any
+		for _, n := range el.Nodes {
+			if n.Leaf() {
+				arr = append(arr, n.Value())
+				continue
+			}
+			arr = append(arr, traverse(n))
+		}
+		return arr
+	}
+	return traverse(i.node)
+}
+
 type NodeList struct {
 	nodes []Node
 }
