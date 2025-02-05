e865b1fff6e532dddb6ad147d8cb44d883cb8e03
midbel <brun.nicolas21@gmail.com>
2025-02-05 21:10:59 +0100

    begin test for case tranformation

---
+++ casing/casing_test.go
@@ -0,0 +1,57 @@
+package casing_test
+
+import (
+	"testing"
+
+	"github.com/midbel/codecs/casing"
+)
+
+func TestCasing(t *testing.T) {
+	data := []struct{
+		Input string
+		Want string
+		Case casing.CaseType
+	}{
+		{
+			Input: "foobar",
+			Want: "foobar",
+			Case: casing.SnakeCase,
+		},
+		{
+			Input: "fooBar",
+			Want: "foo-bar",
+			Case: casing.KebabCase,
+		},
+		{
+			Input: "fooBar",
+			Want: "foo_bar",
+			Case: casing.SnakeCase,
+		},
+		{
+			Input: "fooBAR",
+			Want: "foo_bar",
+			Case: casing.SnakeCase,
+		},
+		{
+			Input: "fooBAR",
+			Want: "foo-bar",
+			Case: casing.KebabCase,
+		},
+		{
+			Input: "foo___-___BAR",
+			Want: "foo-bar",
+			Case: casing.KebabCase,
+		},
+		{
+			Input: "  ---foo_bar---  ",
+			Want: "foo-bar",
+			Case: casing.KebabCase,
+		},
+	}
+	for _, d := range data {
+		got := casing.To(d.Case, d.Input)
+		if got != d.Want {
+			t.Errorf("result mismatched! want %s, got %s", d.Want, got)
+		}
+	}
+}
