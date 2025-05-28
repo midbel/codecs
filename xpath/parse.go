af3b160b4e51528a109c91b66de40fed0c0bec09
midbel <brun.nicolas21@gmail.com>
2025-05-28 21:43:21 +0200

    start import compiler to xpath package

--- xpath/parse.go
+++ xpath/parse.go
@@ -1,5 +1,60 @@
 package xpath
 
+type compiler struct {
+	scan *Scanner
+	curr Token
+	peek Token
+
+	mode       StepMode
+	strictMode bool
+
+	infix  map[rune]func(Expr) (Expr, error)
+	prefix map[rune]func() (Expr, error)
+}
+
 func CompileString(query string) (Expr, error) {
 	return nil, nil
+}
+
+const (
+	powLowest = iota
+	powAssign // variable assignment
+	powOr
+	powAnd
+	powRange
+	powCmp
+	powConcat
+	powAdd
+	powMul
+	powPrefix
+	powAlt    // union
+	powPath // step
+	powPred
+	powCall
+	powHighest
+)
+
+var bindings = map[rune]int{
+	currLevel: powPath,
+	anyLevel:  powPath,
+	opAlt:     powAlt,
+	opConcat:  powConcat,
+	opAssign:  powAssign,
+	opEq:      powCmp,
+	opNe:      powCmp,
+	opGt:      powCmp,
+	opGe:      powCmp,
+	opLt:      powCmp,
+	opLe:      powCmp,
+	opBefore:  powCmp,
+	opAfter:   powCmp,
+	opAnd:     powAnd,
+	opOr:      powOr,
+	opAdd:     powAdd,
+	opSub:     powAdd,
+	opMul:     powMul,
+	opDiv:     powMul,
+	opMod:     powMul,
+	begGrp:    powCall,
+	begPred:   powPred,
 }
