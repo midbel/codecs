451c2a2815708a50b439e817268dc04e576ae955
midbel <brun.nicolas21@gmail.com>
2026-05-17 19:40:22 +0200

    remove unused code

--- json/parse.go
+++ json/parse.go
@@ -11,14 +11,6 @@ import (
 	"unicode/utf8"
 )
 
-var (
-	errUndefined   = errors.New("undefined")
-	errDiscard     = errors.New("discard")
-	errType        = errors.New("type")
-	errArgument    = errors.New("argument")
-	errImplemented = errors.New("not implemented")
-)
-
 type Parser struct {
 	scan *Scanner
 	curr Token
