a373c5ea31339659fc644aa6656169678abfc3b9
midbel <brun.nicolas21@gmail.com>
2026-05-19 21:07:25 +0200

    basic API

--- traversal/traversal.go
+++ traversal/traversal.go
@@ -1,1 +1,36 @@
 package traversal
+
+type Step struct {
+	Field string
+	Cast  string
+}
+
+func Collect(in any, paths []string) ([]any, error) {
+	st := make([]Step, 0, len(paths))
+	for _, p := range paths {
+		s := Step{
+			Field: p,
+		}
+		st = append(st, s)
+	}
+	return collect(in, st)
+}
+
+func collect(in any, paths []Step) ([]any, error) {
+	switch in := in.(type) {
+	case []any:
+		return traverseArray(in, paths)
+	case map[string]any:
+		return traverseObject(in, paths)
+	default:
+		return nil, nil
+	}
+}
+
+func traverseArray(in []any, paths []Step) ([]any, error) {
+	return nil, nil
+}
+
+func traverseObject(in map[string]any, paths []Step) ([]any, error) {
+	return nil, nil
+}
